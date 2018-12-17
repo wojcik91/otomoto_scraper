b105586ba472bb8574265e6fcf232931669781a0
Maciej Wójcik <wojcik91@gmail.com>
2018-12-17 09:16:57 +0100

    Loop through all the consecutive listings pages

---
+++ scraper.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+
+ENTRYPOINT_URL = "https://www.otomoto.pl/osobowe/toyota/auris/?search%5Bbrand_program_id%5D%5B0%5D=&search%5Bcountry%5D=&page=53"
+
+
+def get_page_soup(url):
+    response = requests.get(url)
+    page_soup = BeautifulSoup(response.text, 'html.parser')
+    return page_soup
+
+
+def get_next_page_url(soup):
+    next_button = soup.find('ul', class_='om-pager').find('li', class_='next abs')
+    if(next_button):
+        next_url = next_button.a['href']
+    else:
+        next_url = None
+    return next_url
+
+
+def get_all_listings(soup):
+    listings = soup.find_all('article', class_='adListingItem')
+    return listings
+
+
+if __name__ == "__main__":
+    page_url = ENTRYPOINT_URL
+
+    while(page_url):
+        soup = get_page_soup(page_url)
+        offer_list = get_all_listings(soup)
+        print(len(offer_list))
+        page_url = get_next_page_url(soup)
+        sleep(0.1)
